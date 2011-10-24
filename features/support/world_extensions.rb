996c1255b046b096fde2233249d84f85185cc1b1
Steve Loveless <steve.loveless@gmail.com>
2011-10-24 09:49:44 -0700

    Added #local_ip helper

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -6,6 +6,10 @@ module HelperStuff
   def fake_device_collection
     @fake_device_collection ||= FakeUPnPDeviceCollection.instance
   end
+
+  def local_ip
+    @local_ip ||= local_ip_and_port.first
+  end
 end
 
 World(HelperStuff)
