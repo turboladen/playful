e8aab78ec6012455fc6881c3809f3bc7de4c1787
Steve Loveless <steve.loveless@gmail.com>
2011-10-28 17:09:17 -0700

    Fixed require of old upnp/ssdp

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ SimpleCov.start
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'upnp/ssdp'
+require 'upnp'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
