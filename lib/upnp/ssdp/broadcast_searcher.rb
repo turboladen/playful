d7e0c243b53b9812b355e0ca2190500106f98414
Steve Loveless <steve.loveless@gmail.com>
2012-09-14 17:37:53 -0700

    Fixed indentation

---
+++ lib/upnp/ssdp/broadcast_searcher.rb
@@ -0,0 +1,128 @@
+require_relative '../../core_ext/socket_patch'
+require_relative 'network_constants'
+require 'ipaddr'
+require 'socket'
+require 'eventmachine'
+
+
+module UPnP
+  class SSDP
+    class Connection < EventMachine::Connection
+      include EventMachine::Deferrable
+      include UPnP::SSDP::NetworkConstants
+
+      # @return [Array] The list of responses from the current discovery request.
+      attr_reader :discovery_responses
+
+      attr_reader :available_responses
+      attr_reader :byebye_responses
+
+      def initialize ttl=TTL
+        @ttl = ttl
+        @discovery_responses = []
+        @available_responses = []
+        @byebye_responses = []
+
+        setup_multicast_socket
+      end
+
+      # Gets the IP and port from the peer that just sent data.
+      #
+      # @return [Array<String,Fixnum>] The IP and port.
+      def peer_info
+        peer_bytes = get_peername[2, 6].unpack("nC4")
+        port = peer_bytes.first.to_i
+        ip = peer_bytes[1, 4].join(".")
+
+        [ip, port]
+      end
+
+      def receive_data(response)
+        ip, port = peer_info
+        SSDP.log "<#{self.class}> Response from #{ip}:#{port}:\n#{response}\n"
+        parsed_response = parse(response)
+
+        if parsed_response.has_key? :nts
+          if parsed_response[:nts] == "ssdp:alive"
+            @available_responses << parsed_response
+          elsif parsed_response[:nts] == "ssdp:bye-bye"
+            @byebye_responses << parsed_response
+          else
+            raise "Unknown NTS value: #{parsed_response[:nts]}"
+          end
+        else
+          @discovery_responses << parsed_response
+        end
+      end
+
+      # Converts the headers to a set of key-value pairs.
+      #
+      # @param [String] data The data to convert.
+      # @return [Hash] The converted data.  Returns an empty Hash if it didn't
+      #   know how to parse.
+      def parse(data)
+        new_data = {}
+
+        unless data =~ /\n/
+          SSDP.log "<#{self.class}> Received response as a single-line String.  Discarding."
+          SSDP.log "<#{self.class}> Bad response looked like:\n#{data}"
+          return new_data
+        end
+
+        data.each_line do |line|
+          line =~ /(\S*):(.*)/
+
+          unless $1.nil?
+            key = $1
+            value = $2
+            key = key.gsub('-', '_').downcase.to_sym
+            new_data[key] = value.strip
+          end
+        end
+
+        new_data
+      end
+
+      # Sets Socket options to allow for multicasting.  If ENV["RUBY_UPNP_ENV"] is
+      # equal to "testing", then it doesn't turn off multicast looping.
+      def setup_multicast_socket
+        set_membership(IPAddr.new(BROADCAST).hton + IPAddr.new('0.0.0.0').hton)
+        set_multicast_ttl(@ttl)
+        set_ttl(@ttl)
+
+        unless ENV["RUBY_UPNP_ENV"] == "testing"
+          switch_multicast_loop :off
+        end
+      end
+
+      # @param [String] membership The network byte ordered String that represents
+      #   the IP(s) that should join the membership group.
+      def set_membership(membership)
+        set_sock_opt(Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP, membership)
+      end
+
+      # @param [Fixnum] ttl TTL to set IP_MULTICAST_TTL to.
+      def set_multicast_ttl(ttl)
+        set_sock_opt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_TTL, [ttl].pack('i'))
+      end
+
+      # @param [Fixnum] ttl TTL to set IP_TTL to.
+      def set_ttl(ttl)
+        set_sock_opt(Socket::IPPROTO_IP, Socket::IP_TTL, [ttl].pack('i'))
+      end
+
+      # @param [Symbol] on_off Turn on/off multicast looping.  Supply :on or :off.
+      def switch_multicast_loop(on_off)
+        hex_value = case on_off
+        when :on then "\001"
+        when "\001" then "\001"
+        when :off then "\000"
+        when "\000" then "\000"
+        else raise SSDP::Error, "Can't switch IP_MULTICAST_LOOP to '#{on_off}'"
+                    end
+
+        set_sock_opt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_LOOP, hex_value)
+      end
+    end
+  end
+end
--- lib/upnp/ssdp/searcher.rb
+++ lib/upnp/ssdp/searcher.rb
@@ -1,45 +1,46 @@
 require_relative 'connection'
 
 module UPnP
-# A subclass of an EventMachine::Connection, this handles doing M-SEARCHes.
-#
-# Search types:
-#   ssdp:all
-#   upnp:rootdevice
-#   uuid:[device-uuid]
-#   urn:schemas-upnp-org:device:[deviceType-version]
-#   urn:schemas-upnp-org:service:[serviceType-version]
-class SSDP::Searcher < SSDP::Connection
 
-  # @param [String] search_target
-  # @param [Fixnum] response_wait_time
-  # @param [Fixnum] ttl
-  def initialize(search_target, response_wait_time, ttl)
-    @search = m_search(search_target, response_wait_time)
-    super ttl
-  end
-
-  # Sends the M-SEARCH that was built during init.  Logs what was sent if the
-  # send was successful.
-  def post_init
-    if send_datagram(@search, BROADCAST, MULTICAST_PORT) > 0
-      SSDP.log("Sent datagram search:\n#{@search}")
-    end
-  end
-
-  # Builds the M-SEARCH request string.
+  # A subclass of an EventMachine::Connection, this handles doing M-SEARCHes.
   #
-  # @param [String] search_target
-  # @param [Fixnum] response_wait_time
-  def m_search(search_target, response_wait_time)
-    <<-MSEARCH
+  # Search types:
+  #   ssdp:all
+  #   upnp:rootdevice
+  #   uuid:[device-uuid]
+  #   urn:schemas-upnp-org:device:[deviceType-version]
+  #   urn:schemas-upnp-org:service:[serviceType-version]
+  class SSDP::Searcher < SSDP::Connection
+
+    # @param [String] search_target
+    # @param [Fixnum] response_wait_time
+    # @param [Fixnum] ttl
+    def initialize(search_target, response_wait_time, ttl)
+      @search = m_search(search_target, response_wait_time)
+      super ttl
+    end
+
+    # Sends the M-SEARCH that was built during init.  Logs what was sent if the
+    # send was successful.
+    def post_init
+      if send_datagram(@search, BROADCAST, MULTICAST_PORT) > 0
+        SSDP.log("Sent datagram search:\n#{@search}")
+      end
+    end
+
+    # Builds the M-SEARCH request string.
+    #
+    # @param [String] search_target
+    # @param [Fixnum] response_wait_time
+    def m_search(search_target, response_wait_time)
+      <<-MSEARCH
 M-SEARCH * HTTP/1.1\r
 HOST: #{BROADCAST}:#{MULTICAST_PORT}\r
 MAN: "ssdp:discover"\r
 MX: #{response_wait_time}\r
 ST: #{search_target}\r
 \r
-    MSEARCH
+      MSEARCH
+    end
   end
-end
 end
