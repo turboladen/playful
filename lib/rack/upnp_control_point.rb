702fb2323c11d8111cf1ce826ab850ff6d50117b
Steve Loveless <steve.loveless@gmail.com>
2012-09-25 15:36:16 -0700

    Added docs

--- lib/rack/upnp_control_point.rb
+++ lib/rack/upnp_control_point.rb
@@ -1,15 +1,50 @@
 require 'rack'
+require_relative '../upnp/control_point'
 
 module Rack
+
+  # Middleware that allows your Rack app to keep tabs on devices that the
+  # UPnP::ControlPoint has found.  UPnP devices that match the +search_type+
+  # are discovered and added to the list, then removed as those devices send out
+  # ssdp:byebye notifications.  All of this depends on EventMachine::Queues,
+  # and thus requires that an EventMachine reactor is running.  If you don't
+  # have one running, the UPnP::ControlPoint will start one for you.
+  #
+  # @example Control all root devices
+  #
+  #   Thin::Server.start('0.0.0.0', 3000) do
+  #     use Rack::UPnPControlPoint, search_type: :root
+  #
+  #     map "/devices" do
+  #       run lambda { |env|
+  #         devices = env['upnp.devices']
+  #         friendly_names = devices.map(&:friendly_name).join("\n")
+  #         [200, {'Content-Type' => 'text/plain'}, [friendly_names]]
+  #       }
+  #     end
+  #   end
+  #
   class UPnPControlPoint
-    def initialize(app, search_type)
+
+    # @param [Rack::Builder] app Your Rack application.
+    # @param [Symbol,String] search_type The device(s) you want to search for
+    #   and control.  See docs for UPnP::SSDP::Searcher.
+    # @param [Hash] options Options to pass to the UPnP::SSDP::Searcher.
+    def initialize(app, options={})
       @app = app
       @devices = []
-      EM.next_tick { start_control_point(search_type) }
+      options[:search_type] ||= :root
+      EM.next_tick { start_control_point(options[:search_type], options) }
     end
 
-    def start_control_point(search_type)
-      @cp = UPnP::ControlPoint.new search_type
+    # Creates and starts the UPnP::ControlPoint, then manages the list of devices
+    # using the EventMachine::Queue objects yielded in.
+    #
+    # @param [Symbol,String] search_type The device(s) you want to search for
+    #   and control.  See docs for UPnP::SSDP::Searcher.
+    # @param [Hash] options Options to pass to the UPnP::SSDP::Searcher.
+    def start_control_point(search_type, options)
+      @cp = ::UPnP::ControlPoint.new(search_type, options)
 
       @cp.start do |new_device_queue, old_device_queue|
         new_device_queue.pop { |new_device| @devices << new_device }
@@ -20,6 +55,10 @@ module Rack
       end
     end
 
+    # Adds the whole list of devices to +env['upnp.devices']+ so that that list
+    # can be accessed from within your app.
+    #
+    # @param [Hash] env The Rack environment.
     def call(env)
       env['upnp.devices'] = @devices
       @app.call(env)
