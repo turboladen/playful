fd0f612070a1cd3ba4c371582beca6e884d4bbc0
Steve Loveless <steve.loveless@gmail.com>
2012-10-02 23:26:25 -0700

    Don't need these anymore

--- test_device.rb
+++
@@ -1,4 +0,0 @@
-require './lib/upnp/device'
-
-d = UPnP::Device.new('0.0.0.0', 49159)
-d.start
--- test_listener.rb
+++
@@ -1,8 +0,0 @@
-require './lib/upnp/ssdp'
-
-UPnP::SSDP.log = false
-
-UPnP::SSDP.listen do |responses|
-    puts "testsers got responses: #{responses}"
-    exit unless responses.empty?
-end
