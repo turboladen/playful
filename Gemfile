29d6ec8c7292a733d1f4dcb7baf9db4878d8a2cc
Steve Loveless <steve.loveless@gmail.com>
2013-05-23 15:12:05 -0700

    Trying out coveralls

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 gemspec
 
+gem 'coveralls', require: false
 #gem "httpi", '>=2.0.0.rc1'
 #gem 'em-synchrony'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require 'simplecov'
 
 SimpleCov.start
 
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
